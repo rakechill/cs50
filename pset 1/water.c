f1173c439da5e1f151caf81befbed9824f6be1f5
Rachel Gregory <30397998+rakechill@users.noreply.github.com>
2017-10-28 14:01:13 -0700

    Create water.c

---
+++ pset 1/water.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int n;
+    printf("Minutes: ");
+    do
+    {
+        n = get_int();
+    }
+    while(n <= 0);
+
+    n = n * 12;
+    printf("Bottles: %d\n", n);
+}
+
+
+
+
+
+
+
